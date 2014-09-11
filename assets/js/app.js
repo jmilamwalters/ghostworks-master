3515dc419baaaa9e62a1b828c32936eff73bb374
Jonathan Milam Walters <jonathan@thedeps.com>
2014-09-10 21:11:48 -0500

    Getting close
    Webapps business logic largely concreted;
    Styling close, as per Client’s directives;
    Largest outstanding item includes convergence of ``@media`` queries;
    Then, content management (I hate myself).

--- assets/js/app.js
+++ assets/js/app.js
@@ -132,35 +132,35 @@ $(document).ready(function() {
 	}
 	$subtitle.remove();
 
-	// Make punctuation smarter
-	jQuery.fn.smarten = (function() {
+// Make punctuation smarter
+  jQuery.fn.smarten = (function() {
 
-	  function smartenNode(node) {
-		if (node.nodeType === 3) {
-		  node.data = node.data
-			.replace(/(^|[-\u2014/(\[{"\s])'/g, "$1\u2018")      // Opening singles
-			.replace(/'/g, "\u2019")                             // Closing singles & apostrophes
-			.replace(/(^|[-\u2014/(\[{\u2018\s])"/g, "$1\u201c") // Opening doubles
-			.replace(/"/g, "\u201d")                             // Closing doubles
-			.replace(/--/g, "\u2013")                            // En dashes
-			.replace(/---/g, "\u2014")                           // Em dashes
-			.replace(/\.{3}/g, "\u2026");                        // Ellipsis
-		} else if (node.nodeType === 1) {
-		  if (node = node.firstChild) do {
-			smartenNode(node);
-		  } while (node = node.nextSibling);
-		}
-	  }
+    function smartenNode(node) {
+    if (node.nodeType === 3) {
+      node.data = node.data
+      .replace(/(^|[-\u2014/(\[{"\s])'/g, "$1\u2018")      // Opening singles
+      .replace(/'/g, "\u2019")                             // Closing singles & apostrophes
+      .replace(/(^|[-\u2014/(\[{\u2018\s])"/g, "$1\u201c") // Opening doubles
+      .replace(/"/g, "\u201d")                             // Closing doubles
+      .replace(/--/g, "\u2013")                            // En dashes
+      .replace(/---/g, "\u2014")                           // Em dashes
+      .replace(/\.{3}/g, "\u2026");                        // Ellipsis
+    } else if (node.nodeType === 1) {
+      if (node = node.firstChild) do {
+      smartenNode(node);
+      } while (node = node.nextSibling);
+    }
+    }
 
-	  return function() {
-		return this.each(function(){
-		  smartenNode(this);
-		});
-	  };
+    return function() {
+    return this.each(function(){
+      smartenNode(this);
+    });
+    };
 
-	}());
+  }());
 
-	// Instantiation
-	$('article').smarten();
+  // Instantiation
+  $('article').smarten();
 
 });
